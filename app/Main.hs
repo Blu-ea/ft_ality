7437b732bd1cfee230a97f1de73428630291c74a
Isioblast <th.dv74@gmail.com>
2025-12-15 21:10:47 +0100

    show action in main loop

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,8 @@
 module Main where
 
+import System.Environment
+import System.Directory (doesFileExist)
+
 import SDL
     ( Keycode,
       initialize,
@@ -7,13 +10,18 @@ import SDL
       defaultWindow,
       destroyWindow,
       InitFlag(InitJoystick, InitEvents, InitGameController) )
-import Data.Text(pack)
-
 import SDL.Input.GameController ( availableControllers )
+import SDL.Internal.Numbered (FromNumber(..))
+
+import Data.Text(pack)
+import Data.Foldable (find)
+
 import EventListner ( getEventFilter, reduceEventList )
-import SDL.Internal.Numbered (FromNumber(fromNumber), ToNumber(toNumber))
-import System.Environment
-import System.Directory (doesFileExist)
+
+import Parsing.FileMachine as FM
+import Machine.StateMachine (AlityMachine(..), State, Action, deltaFunction, initialStateId)
+import Parsing.AlityParser as Parser
+import Utils
 
 data ArgType = Error | Help | File String deriving (Show, Eq)
 
@@ -41,28 +49,46 @@ runProgram filePath = do
     if not fileExist
         then putStrLn $ "Error: File " ++ filePath ++ " does not exist."
         else do
-            initialize [InitEvents, InitGameController, InitJoystick]
 
-            window <- createWindow  (pack "ft_ality | Key1-detector") defaultWindow
-            -- renderer <- createRenderer window (-1) defaultRenderer
+            fileStr <- readFile filePath
+            let (bindings, machine) = FM.stringToMachine fileStr deltaFunction -- TODO error handling
+                bindingsCode = map (\(Parser.KeyBinding name action) -> (nameToKeycode name, action)) bindings
+                initialMachineState = find (\ (stateID, _, _) -> stateID == initialStateId machine) (states machine)
 
-            test <- availableControllers
-            print test
+            case initialMachineState of
+                Nothing -> putStrLn "Error: Initial state not found in machine states."
+                Just initialState -> do
 
-            ls <- getEventFilter $ map fromNumber [97 .. 122] -- Get all the letters
-            process $ reduceEventList ls
+                    initialize [InitEvents, InitGameController, InitJoystick]
+                    window <- createWindow  (pack "ft_ality | Key1-detector") defaultWindow
+                    -- renderer <- createRenderer window (-1) defaultRenderer
 
-            destroyWindow window
+                    test <- availableControllers
+                    print test
 
-process :: [[Keycode]] -> IO ()
-process (x:xs) = do
-    process' x
-    process xs
-process [] = print "Ending the process !"
+                    ls <- getEventFilter $ map fromNumber [97 .. 122] -- Get all the letters
+                    processLoop (reduceEventList ls) (bindingsCode, machine, initialState)
 
+                    destroyWindow window
 
-process' :: [Keycode] -> IO()
-process' (x:xs) = do
-    print $ toNumber x
-    process' xs
-process' [] = print "--- new input ---"
+processLoop :: [[Keycode]] -> ([(Keycode, Action)], AlityMachine, State) -> IO ()
+processLoop [] _ = print "Quitting..."
+processLoop (x:xs) (bindings, machine, (currentId, _, _)) = do
+    let lst = keycodesToActions x bindings
+    print $ "current actions : " ++ show lst
+    let newStateId = delta machine machine currentId lst
+        in case newStateId of
+            Nothing -> putStrLn $ "No transition found from state id " ++ show currentId ++ " with actions " ++ show lst
+            Just newId ->
+                let nextState = find (\ (stateID, _, _) -> stateID == newId) (states machine)
+                in case nextState of
+                    Nothing -> putStrLn $ "No transition found from state id " ++ show currentId ++ " with actions " ++ show lst
+                    Just ns@(_, _, nextCombo) -> do
+                        putStrLn $ "Transitioning to state id " ++ show newId ++ " with combos :\n"
+                        mapM_ (\(charName, combo) -> putStrLn $ "- " ++ combo ++ " (" ++ charName ++ ")") nextCombo
+                        processLoop xs (bindings, machine, ns)
+
+keycodesToActions :: [Keycode] -> [(Keycode, Action)] -> [String]
+keycodesToActions keycodes bindings =
+    [ action | kc <- keycodes, (bindKc, action) <- bindings, kc == bindKc ]
+
