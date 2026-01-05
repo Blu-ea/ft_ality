936443ecab0458449ff626d763e1f81a59f58080
blu-ea <antoine.miguez@gmail>
2026-01-05 20:13:02 +0100

    Fix: Issue on pc where pullEvent doesn't work

--- app/EventListner.hs
+++ app/EventListner.hs
@@ -1,28 +1,25 @@
 module EventListner where
 
 import qualified SDL
-import GHC.IO (unsafeInterleaveIO)
-import Data.Maybe (isNothing, fromJust)
+import System.IO.Unsafe (unsafeInterleaveIO)
 import Utils ( isKeyPress, isKeyRelease )
 import Data.List ( delete )
 
 getEventFilter :: [SDL.Keycode] -> IO[SDL.Event]
 getEventFilter inputKeys = unsafeInterleaveIO $ do
-    event <- SDL.pollEvent
+    event <- SDL.waitEvent
     events <- getEventFilter inputKeys
     
-    if isNothing event then return events else 
-        let e = fromJust event in 
-        case SDL.eventPayload e of
-            SDL.KeyboardEvent ke | isEscapeKey ke
-                -> return []
-            SDL.WindowClosedEvent _ 
-                -> return []
+    case SDL.eventPayload event of
+        SDL.KeyboardEvent ke | isEscapeKey ke
+            -> return []
+        SDL.WindowClosedEvent _ 
+            -> return []
 
-            SDL.KeyboardEvent ke | isInputKey ke && (isKeyPress ke || isKeyRelease ke)
-                -> return (e : events)
+        SDL.KeyboardEvent ke | isInputKey ke && (isKeyPress ke || isKeyRelease ke)
+            -> return (event : events)
 
-            _ -> return events
+        _ -> return events
     where
     isInputKey ke = SDL.keysymKeycode(SDL.keyboardEventKeysym ke) `elem` inputKeys
     isEscapeKey ke = SDL.keysymKeycode (SDL.keyboardEventKeysym ke) == SDL.KeycodeEscape 
@@ -53,21 +50,3 @@ reduceEventList' (e:es) keyPressed acc = case SDL.eventPayload e of
                 else res acc
 
     _ -> reduceEventList' es keyPressed acc
-
-
-    -- -- When Ctl+c is pressed, require an other input to leave the processe
-    -- event <- waitEvent
-    -- events <- getEventFilter
-    -- case eventPayload event of
-    --     paylaod@(KeyboardEvent _)| t paylaod
-    --             -> pure (event : events)
-    --     _ -> pure events
-    --   --If end -> return []
-    -- where t _ = True
-
-
-    -- -- Doesn't work corectlly because i want to leave if i found an exit event, but there, it will still continue to loop
-    -- event <- pollEvents
-    -- events <- getEventFilter
-    -- return $ filter t event ++ events
-    -- where t _ = True
